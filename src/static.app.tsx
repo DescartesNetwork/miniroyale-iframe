25f3e79f0f1a28da01bbf420cce75b2867f96c51
tuphan-dn <tuphan@descartes.network>
2022-06-30 10:36:19 +0700

    update v3

---
+++ src/static.app.tsx
@@ -0,0 +1,7 @@
+import panel1 from 'static/images/panel-1.png'
+import panel2 from 'static/images/panel-2.png'
+import panel3 from 'static/images/panel-3.png'
+
+export const panels = [panel1, panel2, panel3]
+export { default as logo } from 'static/images/logo.png'
+export { default as readme } from 'static/docs/README.md'
